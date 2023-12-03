85302f4d0d923ce908399a307da56964999c2cce
Vasilii Razdorskii <vrazdorsky@yandex.ru>
2023-12-03 12:26:50 +0300

    test commit

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,10 +3,9 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import leavesBackgroundSrc from '../../assets/leaves_background.svg';
 import awardCupBackgroundSrc from '../../assets/award_cup_background.svg';
 import mireaLogosBackgroundSrc from '../../assets/mirea_logos_background.svg';
-
-import Banner from '../Banner';
 import awardCup from '../../assets/awardCup.svg';
 import bannerTextDesktopSrc from '../../assets/banner_text_desktop.svg';
+import arrowDownSrc from '../../assets/arrow_down.svg';
 
 const queryClient = new QueryClient();
 
@@ -29,7 +28,12 @@ export default function App() {
 					<img src={bannerTextDesktopSrc} className={styles.bannerText} />
 				</div>
 
-				{/* <Banner /> */}
+				<button className={styles.downButton}>
+					<p className={styles.downButtonText}>
+						Нажмите, чтобы продолжить просмотр
+					</p>
+					<img src={arrowDownSrc} className={styles.downButtonIcon} />
+				</button>
 			</main>
 		</QueryClientProvider>
 	);
