fee89e355ab7525b3d5a348a3b9a76984e0d8130
Vasilii Razdorskii <vrazdorsky@yandex.ru>
2023-12-03 13:12:31 +0300

    implement mobile banner version

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,11 +5,18 @@ import awardCupBackgroundSrc from '../../assets/award_cup_background.svg';
 import mireaLogosBackgroundSrc from '../../assets/mirea_logos_background.svg';
 import awardCup from '../../assets/awardCup.svg';
 import bannerTextDesktopSrc from '../../assets/banner_text_desktop.svg';
+import bannerTextMobileSrc from '../../assets/banner_text_mobile.svg';
 import arrowDownSrc from '../../assets/arrow_down.svg';
+import { useState } from 'react';
 
 const queryClient = new QueryClient();
 
 export default function App() {
+	const [currentWidth, setCurrentWidth] = useState(window.innerWidth);
+
+	window.addEventListener('resize', () => {
+		setCurrentWidth(window.innerWidth);
+	});
 	return (
 		<QueryClientProvider client={queryClient}>
 			<main className={styles.main}>
@@ -25,15 +32,28 @@ export default function App() {
 				/>
 				<div className={styles.mainContent}>
 					<img src={awardCup} className={styles.bannerAwardCup} />
-					<img src={bannerTextDesktopSrc} className={styles.bannerText} />
+					{currentWidth > 1024 ? (
+						<img src={bannerTextDesktopSrc} className={styles.bannerText} />
+					) : (
+						<img
+							src={bannerTextMobileSrc}
+							className={styles.bannerTextMobile}
+						/>
+					)}
+
+					{currentWidth <= 1024 && (
+						<img src={arrowDownSrc} className={styles.downButtonIconMobile} />
+					)}
 				</div>
 
-				<button className={styles.downButton}>
-					<p className={styles.downButtonText}>
-						Нажмите, чтобы продолжить просмотр
-					</p>
-					<img src={arrowDownSrc} className={styles.downButtonIcon} />
-				</button>
+				{currentWidth > 1024 && (
+					<button className={styles.downButton}>
+						<p className={styles.downButtonText}>
+							Нажмите, чтобы продолжить просмотр
+						</p>
+						<img src={arrowDownSrc} className={styles.downButtonIcon} />
+					</button>
+				)}
 			</main>
 		</QueryClientProvider>
 	);
