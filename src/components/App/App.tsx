83bbf77892c636df1b720f4b35c4b0bc659d3e83
Vasilii Razdorskii <vrazdorsky@yandex.ru>
2023-12-03 13:19:54 +0300

    move main banner content to Banner component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,11 +3,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import leavesBackgroundSrc from '../../assets/leaves_background.svg';
 import awardCupBackgroundSrc from '../../assets/award_cup_background.svg';
 import mireaLogosBackgroundSrc from '../../assets/mirea_logos_background.svg';
-import awardCup from '../../assets/awardCup.svg';
-import bannerTextDesktopSrc from '../../assets/banner_text_desktop.svg';
-import bannerTextMobileSrc from '../../assets/banner_text_mobile.svg';
-import arrowDownSrc from '../../assets/arrow_down.svg';
 import { useState } from 'react';
+import Banner from '../Banner';
 
 const queryClient = new QueryClient();
 
@@ -30,30 +27,8 @@ export default function App() {
 					src={mireaLogosBackgroundSrc}
 					className={styles.mireaLogosBackground}
 				/>
-				<div className={styles.mainContent}>
-					<img src={awardCup} className={styles.bannerAwardCup} />
-					{currentWidth > 1024 ? (
-						<img src={bannerTextDesktopSrc} className={styles.bannerText} />
-					) : (
-						<img
-							src={bannerTextMobileSrc}
-							className={styles.bannerTextMobile}
-						/>
-					)}
 
-					{currentWidth <= 1024 && (
-						<img src={arrowDownSrc} className={styles.downButtonIconMobile} />
-					)}
-				</div>
-
-				{currentWidth > 1024 && (
-					<button className={styles.downButton}>
-						<p className={styles.downButtonText}>
-							Нажмите, чтобы продолжить просмотр
-						</p>
-						<img src={arrowDownSrc} className={styles.downButtonIcon} />
-					</button>
-				)}
+				<Banner currentWidth={currentWidth} />
 			</main>
 		</QueryClientProvider>
 	);
--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -2,15 +2,37 @@ import styles from './Banner.module.scss';
 
 import awardCup from '../../assets/awardCup.svg';
 import bannerTextDesktopSrc from '../../assets/banner_text_desktop.svg';
+import bannerTextMobileSrc from '../../assets/banner_text_mobile.svg';
+import arrowDownSrc from '../../assets/arrow_down.svg';
 
-export function Banner() {
+interface BannerProps {
+	currentWidth: number;
+}
+
+export function Banner({ currentWidth }: BannerProps) {
 	return (
-		<section className={styles.banner}>
-			<div className={styles.bannerMainContent}>
+		<>
+			<div className={styles.mainContent}>
 				<img src={awardCup} className={styles.bannerAwardCup} />
-				<img src={bannerTextDesktopSrc} className={styles.bannerText} />
+				{currentWidth > 1024 ? (
+					<img src={bannerTextDesktopSrc} className={styles.bannerText} />
+				) : (
+					<img src={bannerTextMobileSrc} className={styles.bannerTextMobile} />
+				)}
+
+				{currentWidth <= 1024 && (
+					<img src={arrowDownSrc} className={styles.downButtonIconMobile} />
+				)}
 			</div>
-			<button className={styles.bannerDownButton}></button>
-		</section>
+
+			{currentWidth > 1024 && (
+				<button className={styles.downButton}>
+					<p className={styles.downButtonText}>
+						Нажмите, чтобы продолжить просмотр
+					</p>
+					<img src={arrowDownSrc} className={styles.downButtonIcon} />
+				</button>
+			)}
+		</>
 	);
 }
