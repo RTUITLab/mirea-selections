7cec381374f3f6e4a282b312245c0ec963fca8e3
Vasilii Razdorskii <vrazdorsky@yandex.ru>
2023-12-08 15:37:40 +0300

    add loader

--- src/components/Selection/Selection.tsx
+++ src/components/Selection/Selection.tsx
@@ -38,32 +38,36 @@ export function Selection() {
 		},
 	});
 
-	const { data: teachersNominationInfo } = useQuery({
-		queryKey: ['teachersNominationInfo', votingInfo, teacherNominationId],
-		queryFn: async () => {
-			const res = await getNominationInfo(
-				'Bearer ',
-				votingInfo?.id,
-				teacherNominationId
-			);
-			setChosenApplicant(res.data.nominants[0]);
-			return res.data;
-		},
-		enabled: !!teacherNominationId && !!votingInfo,
-	});
+	const { data: teachersNominationInfo, isLoading: teachersLoading } = useQuery(
+		{
+			queryKey: ['teachersNominationInfo', votingInfo, teacherNominationId],
+			queryFn: async () => {
+				const res = await getNominationInfo(
+					'Bearer ',
+					votingInfo?.id,
+					teacherNominationId
+				);
+				setChosenApplicant(res.data.nominants[0]);
+				return res.data;
+			},
+			enabled: !!teacherNominationId && !!votingInfo,
+		}
+	);
 
-	const { data: studentsNominationInfo } = useQuery({
-		queryKey: ['studentsNominationInfo', votingInfo, studentNominationId],
-		queryFn: async () => {
-			const res = await getNominationInfo(
-				'Bearer ',
-				votingInfo?.id,
-				studentNominationId
-			);
-			return res.data;
-		},
-		enabled: !!teacherNominationId && !!votingInfo,
-	});
+	const { data: studentsNominationInfo, isLoading: studentsLoading } = useQuery(
+		{
+			queryKey: ['studentsNominationInfo', votingInfo, studentNominationId],
+			queryFn: async () => {
+				const res = await getNominationInfo(
+					'Bearer ',
+					votingInfo?.id,
+					studentNominationId
+				);
+				return res.data;
+			},
+			enabled: !!teacherNominationId && !!votingInfo,
+		}
+	);
 
 	useEffect(() => {
 		if (selectedNomination === 'student') {
@@ -81,102 +85,108 @@ export function Selection() {
 			<div className={styles.selectionContainer}>
 				<img src={selectionTitleSrc} className={styles.selectionTitle} />
 
-				<div className={styles.selectionContent}>
-					<div className={styles.selectionApplicantsListContainer}>
-						<nav className={styles.selectionNominations}>
-							<button
-								className={`${styles.selectionNomination} ${
-									styles.selectionNominationLeft
-								} ${
-									selectedNomination === 'teacher' &&
-									styles.selectionNominationActive
-								}`}
-								onClick={() => setSelectedNomination('teacher')}
-							>
-								ПРЕПОДАВАТЕЛИ
-							</button>
-							<button
-								className={`${styles.selectionNomination} ${
-									styles.selectionNominationRight
-								} ${
-									selectedNomination === 'student' &&
-									styles.selectionNominationActive
-								}`}
-								onClick={() => setSelectedNomination('student')}
-							>
-								СТУДЕНТЫ
-							</button>
-						</nav>
-
-						<div className={styles.selectionApplicantsList}>
-							{selectedNomination === 'teacher'
-								? teachersNominationInfo?.nominants.map((applicant) => {
-										return (
-											<Applicant
-												key={applicant.id}
-												fio={applicant.title}
-												avatarSrc={applicant.cover_url}
-												smallDescription={applicant.short_description}
-												onClick={() => {
-													setChosenApplicant(applicant);
-												}}
-											/>
-										);
-								  })
-								: studentsNominationInfo?.nominants.map((applicant) => {
-										return (
-											<Applicant
-												key={applicant.id}
-												fio={applicant.title}
-												avatarSrc={applicant.cover_url}
-												smallDescription={applicant.short_description}
-												onClick={() => setChosenApplicant(applicant)}
-											/>
-										);
-								  })}
-						</div>
+				{teachersLoading && studentsLoading ? (
+					<div className={styles.selectionLoaderContainer}>
+						<div className={styles.selectionLoader}></div>
 					</div>
+				) : (
+					<div className={styles.selectionContent}>
+						<div className={styles.selectionApplicantsListContainer}>
+							<nav className={styles.selectionNominations}>
+								<button
+									className={`${styles.selectionNomination} ${
+										styles.selectionNominationLeft
+									} ${
+										selectedNomination === 'teacher' &&
+										styles.selectionNominationActive
+									}`}
+									onClick={() => setSelectedNomination('teacher')}
+								>
+									ПРЕПОДАВАТЕЛИ
+								</button>
+								<button
+									className={`${styles.selectionNomination} ${
+										styles.selectionNominationRight
+									} ${
+										selectedNomination === 'student' &&
+										styles.selectionNominationActive
+									}`}
+									onClick={() => setSelectedNomination('student')}
+								>
+									СТУДЕНТЫ
+								</button>
+							</nav>
 
-					{chosenApplicant?.id && (
-						<div className={styles.selectionApplicantInfoContainer}>
-							<div className={styles.selectionApplicantContent}>
-								<img
-									className={styles.selectionApplicantAvatar}
-									src={chosenApplicant.cover_url}
-								/>
-
-								<iframe
-									src={`https://vk.com/video_ext.php?oid=-${chosenApplicant.video_url.slice(
-										21,
-										30
-									)}&id=${chosenApplicant.video_url.slice(31, 40)}&hd=2`}
-									width="1920"
-									height="1080"
-									allow="autoplay; encrypted-media; fullscreen; picture-in-picture;"
-									allowFullScreen
-									className={styles.selectionApplicantVideo}
-								></iframe>
+							<div className={styles.selectionApplicantsList}>
+								{selectedNomination === 'teacher'
+									? teachersNominationInfo?.nominants.map((applicant) => {
+											return (
+												<Applicant
+													key={applicant.id}
+													fio={applicant.title}
+													avatarSrc={applicant.cover_url}
+													smallDescription={applicant.short_description}
+													onClick={() => {
+														setChosenApplicant(applicant);
+													}}
+												/>
+											);
+									  })
+									: studentsNominationInfo?.nominants.map((applicant) => {
+											return (
+												<Applicant
+													key={applicant.id}
+													fio={applicant.title}
+													avatarSrc={applicant.cover_url}
+													smallDescription={applicant.short_description}
+													onClick={() => setChosenApplicant(applicant)}
+												/>
+											);
+									  })}
 							</div>
+						</div>
 
-							<p className={styles.selectionApplicantFIO}>
-								{chosenApplicant.title}
-							</p>
+						{chosenApplicant?.id && (
+							<div className={styles.selectionApplicantInfoContainer}>
+								<div className={styles.selectionApplicantContent}>
+									<img
+										className={styles.selectionApplicantAvatar}
+										src={chosenApplicant.cover_url}
+									/>
 
-							<p className={styles.selectionApplicantDescriptionSmall}>
-								{chosenApplicant.short_description}
-							</p>
+									<iframe
+										src={`https://vk.com/video_ext.php?oid=-${chosenApplicant.video_url.slice(
+											21,
+											30
+										)}&id=${chosenApplicant.video_url.slice(31, 40)}&hd=2`}
+										width="1920"
+										height="1080"
+										allow="autoplay; encrypted-media; fullscreen; picture-in-picture;"
+										allowFullScreen
+										className={styles.selectionApplicantVideo}
+									></iframe>
+								</div>
 
-							<p className={styles.selectionApplicantDescription}>
-								{chosenApplicant.description}
-							</p>
+								<p className={styles.selectionApplicantFIO}>
+									{chosenApplicant.title}
+								</p>
 
-							{/* <button className={styles.selectionVoteButton}>
+								<p className={styles.selectionApplicantDescriptionSmall}>
+									{chosenApplicant.short_description}
+								</p>
+
+								<p className={styles.selectionApplicantDescription}>
+									{chosenApplicant.description}
+								</p>
+
+								{/* <button className={styles.selectionVoteButton}>
 								Проголосовать
 							</button> */}
-							<VoteButton location={'selection'} />
-						</div>
-					)}
-				</div>
+								<VoteButton location={'selection'} />
+							</div>
+						)}
+					</div>
+				)}
 
 				<p className={styles.selectionAnnotation}>
 					*Вы можете отдать голос только за одного претендента в номинации
